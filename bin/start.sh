729ae56a37d8734a7351ed243df6b9b46c266dc3
Ubuntu <ubuntu@ip-172-31-7-148.us-east-2.compute.internal>
2018-01-13 20:00:28 +0000

    Adjusted start.sh with removing old logs

--- bin/start.sh
+++ bin/start.sh
@@ -1,1 +1,2 @@
+rm logs/*
 nohup java -Dserver.port=8081 -javaagent:./jmx_prometheus_javaagent-0.1.0.jar=5531:agent.yaml -server -Xmx5g -XX:+UseG1GC -XX:+PrintGCDetails -XX:+PrintGCDateStamps -XX:+PrintGCTimeStamps -XX:+PrintGCApplicationStoppedTime -XX:+PrintGCCause -XX:+PrintTenuringDistribution -Xloggc:logs/gc.log -jar ../target/demo-0.0.1-SNAPSHOT.jar &
