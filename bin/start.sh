c826f476dbd8b0d2cf68bedc348bb3de13df036b
Ubuntu <ubuntu@ip-172-31-7-148.us-east-2.compute.internal>
2018-01-18 15:53:26 +0000

    Increased connection count in pool

--- bin/start.sh
+++ bin/start.sh
@@ -1,2 +1,3 @@
 rm logs/*
 nohup java -Dserver.port=8081 -javaagent:./jmx_prometheus_javaagent-0.1.0.jar=5531:agent.yaml -server -XX:+PrintFlagsFinal -Xmx5g -XX:+UseG1GC -XX:+PrintGCDetails -XX:+PrintGCDateStamps -XX:+PrintGCTimeStamps -XX:+PrintGCApplicationStoppedTime -XX:+PrintGCCause -XX:+PrintTenuringDistribution -verbose:gc -Xloggc:logs/gc.log -jar ../target/demo-0.0.1-SNAPSHOT.jar &
+ps -aux | grep java
