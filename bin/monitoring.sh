bb61fbd1fdb6b83aca47d2507be027fb3a8e9cd0
Ubuntu <ubuntu@ip-172-31-0-113.us-east-2.compute.internal>
2018-01-18 05:05:49 +0000

    Enhanced monitoring script

--- bin/monitoring.sh
+++ bin/monitoring.sh
@@ -1,2 +1,3 @@
 nohup sudo node_exporter/node_exporter --web.listen-address=":9100" &
 nohup mysqld_exporter/mysqld_exporter -config.my-cnf="mysqld_exporter/config" --web.listen-address=":9104" &
+ps -aux | grep exporter
